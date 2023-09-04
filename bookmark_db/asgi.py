5f11201509bba48ed1ef4d3bce03b215cea649b4
Gabi Nagy <gabrian.nagy@gmail.com>
2023-09-04 18:52:59 +0200

    Add empty Django project

---
+++ bookmark_db/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for bookmark_db project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookmark_db.settings')
+
+application = get_asgi_application()
