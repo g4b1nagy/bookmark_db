241291c44d3322fb2fbef7f0982e126c91a359a7
Gabi Nagy <gabrian.nagy@gmail.com>
2023-09-05 07:09:34 +0200

    Add Label, Bookmark models

--- bookmarks/admin.py
+++ bookmarks/admin.py
@@ -1,3 +1,66 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from django.utils.translation import gettext as _
 
-# Register your models here.
+from .models import Label, Bookmark
+
+
+class BaseModelAdmin(admin.ModelAdmin):
+
+    # Show most recent objects first.
+    ordering = ['-id']
+
+    # The default value is '-'.
+    empty_value_display = ''
+
+    def __init__(self, model, admin_site):
+        super().__init__(model, admin_site)
+
+        # Make all list_display fields clickable.
+        self.list_display_links = self.list_display
+
+
+@admin.register(Label)
+class LabelAdmin(BaseModelAdmin):
+    search_fields = [
+        'name',
+    ]
+    list_display = [
+        'name',
+    ]
+    readonly_fields = [
+        'created_on',
+        'updated_on',
+    ]
+
+
+@admin.register(Bookmark)
+class BookmarkAdmin(BaseModelAdmin):
+    search_fields = [
+        'name',
+        'url',
+        'labels__name',
+    ]
+    list_display = [
+        '_icon',
+        'name',
+        'url',
+    ]
+    list_filter = [
+        'labels__name',
+    ]
+    readonly_fields = [
+        'created_on',
+        'updated_on',
+    ]
+    autocomplete_fields = [
+        'labels',
+    ]
+
+    @admin.display(description=_('icon'))
+    def _icon(self, obj):
+        if obj.icon.name == '':
+            url = ''
+        else:
+            url = obj.icon.url
+        return format_html('<img style="width: auto; max-height: 16px;" src="{url}">', url=url)
--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Label(models.Model):
+    created_on = models.DateTimeField(blank=True, null=True)
+    updated_on = models.DateTimeField(blank=True, null=True)
+
+    name = models.CharField(max_length=256, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Bookmark(models.Model):
+    created_on = models.DateTimeField(blank=True, null=True)
+    updated_on = models.DateTimeField(blank=True, null=True)
+
+    icon = models.FileField(blank=True)
+    name = models.CharField(max_length=256)
+    url = models.URLField(max_length=2048, unique=True)
+    labels = models.ManyToManyField(Label, blank=True)
+
+    def __str__(self):
+        return self.name
