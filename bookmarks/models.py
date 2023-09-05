614ee4cc440f2b9c7a2f7304ad2f380e90417b0b
Gabi Nagy <gabrian.nagy@gmail.com>
2023-09-05 07:12:45 +0200

    Auto-truncate name fields

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 
 
+class TruncatingCharField(models.CharField):
+    def get_prep_value(self, value):
+        value = super(TruncatingCharField, self).get_prep_value(value)
+        if value is not None:
+            if len(value) > self.max_length:
+                value = value[: self.max_length - 3] + '...'
+        return value
+
+
 class Label(models.Model):
     created_on = models.DateTimeField(blank=True, null=True)
     updated_on = models.DateTimeField(blank=True, null=True)
 
-    name = models.CharField(max_length=256, unique=True)
+    name = TruncatingCharField(max_length=256, unique=True)
 
     def __str__(self):
         return self.name
@@ -16,7 +25,7 @@ class Bookmark(models.Model):
     updated_on = models.DateTimeField(blank=True, null=True)
 
     icon = models.FileField(blank=True)
-    name = models.CharField(max_length=256)
+    name = TruncatingCharField(max_length=256)
     url = models.URLField(max_length=2048, unique=True)
     labels = models.ManyToManyField(Label, blank=True)
 
